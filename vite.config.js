0a13d7d88729cadb6661298ad49a598453e8bde3
mohnish2908 <mohnishpanwar29@gmail.com>
2025-04-04 18:22:44 +0530

    vite.config update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: '../',         // ⬅️ Output the build files to root directory
+    emptyOutDir: false     // ⬅️ Don't delete the server folder during build
+  }
 })
